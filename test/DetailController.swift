e747e47475f35997d510cb8665a71040a9db88e2
Hyeonjiwon <zlone1208@gmail.com>
2021-03-06 22:11:19 +0900

    first commit

---
+++ test/DetailController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailController.swift
+//  test
+//
+//  Created by 현지원 on 2021/03/06.
+//
+
+import UIKit
+
+class DetailController : UIViewController {
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
--- test/ViewController.swift
+++ test/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func Click_moveBtn(_ sender: UIButton) {
+        // storyboard find controller : DetailController
+        
+        // 변수 생성, nil(null), 옵셔널 바인딩
+        if let controller = self.storyboard?.instantiateViewController(withIdentifier: "DetailController") {
+            
+            // push controller -> navi
+            self.navigationController?.pushViewController(controller, animated: true)
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
